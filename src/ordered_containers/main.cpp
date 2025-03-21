a8608cdd72bfe04d2c782960635ea40fa3dbb2b3
ahmad <ahmad.alhusein2@gmail.com>
2025-03-22 01:28:34 +0300

    added data generator

--- src/ordered_containers/main.cpp
+++ src/ordered_containers/main.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <chrono>
+#include <climits>
 #include <random>
 #include <type_traits>
 #include <vector>
@@ -10,21 +11,39 @@
 #include "trie_int.h"
 
 std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-int getRand(int l, int r) {
+int get_rand(int l, int r) {
     std::uniform_int_distribution<int> uid(l, r);
     return uid(rng);
 }
 
-template<class T>
+
+class DataGenerator
+{
+public:
+    static std::vector<int> generate(int size, int min_value = INT_MIN, int max_value = INT_MAX);
+};
+
+struct RandomDataGenerator : DataGenerator
+{
+    static std::vector<int> generate(int size, int min_value = INT_MIN, int max_value = INT_MAX)
+    {
+        std::vector<int> vec(size);
+        for (auto &v:vec){
+            v = get_rand(min_value, max_value);
+        }
+        return vec;
+    }
+};
+
+
+template<class T, class TGenerator>
 static void BM_OrderedContainer(benchmark::State &state) {
     static_assert(std::is_base_of<OrderedContainer<int>, T>::value, "should be derived from ordered container class");
 
-    std::vector<int> vec(state.range(0));
-    for (auto &v: vec) {
-        v = getRand(0, (1 << 20));
-    }
-
     for (auto _: state) {
+        state.PauseTiming();
+        std::vector<int> vec = TGenerator::generate(state.range(0));
+        state.ResumeTiming();
         T ordered_container;
         for (auto &v: vec) {
             if (ordered_container.contains(v)) {
@@ -34,30 +53,30 @@ static void BM_OrderedContainer(benchmark::State &state) {
             }
         }
     }
-}
+};
 
-BENCHMARK(BM_OrderedContainer<Set<int>>)
+BENCHMARK_TEMPLATE(BM_OrderedContainer, Set<int>, RandomDataGenerator)
         ->Name("Integers set")
         ->Unit(benchmark::kMillisecond)
         ->Arg((1 << 5))
         ->Arg((1 << 10))
         ->Arg((1 << 18));
 
-BENCHMARK(BM_OrderedContainer<SegmentTreeSet>)
-        ->Name("Segment tree")
-        ->Unit(benchmark::kMillisecond)
-        ->Arg((1 << 5))
-        ->Arg((1 << 10))
-        ->Arg((1 << 18));
+// BENCHMARK_TEMPLATE(BM_OrderedContainer, SegmentTreeSet, RandomDataGenerator)
+//         ->Name("Segment tree")
+//         ->Unit(benchmark::kMillisecond)
+//         ->Arg((1 << 5))
+//         ->Arg((1 << 10))
+//         ->Arg((1 << 18));
 
-BENCHMARK(BM_OrderedContainer<DynamicSegmentTree>)
+BENCHMARK_TEMPLATE(BM_OrderedContainer, DynamicSegmentTree, RandomDataGenerator)
         ->Name("Dynamic segment tree")
         ->Unit(benchmark::kMillisecond)
         ->Arg((1 << 5))
         ->Arg((1 << 10))
         ->Arg((1 << 18));
 
-BENCHMARK(BM_OrderedContainer<TrieInt>)
+BENCHMARK_TEMPLATE(BM_OrderedContainer, TrieInt, RandomDataGenerator)
         ->Name("Integers trie")
         ->Unit(benchmark::kMillisecond)
         ->Arg((1 << 5))
